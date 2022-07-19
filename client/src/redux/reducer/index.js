3f22e19c8a4de1976a2c3f0db6339b533d51325a
leobravo5 <leandrobravo511@gmail.com>
2022-07-19 11:34:35 -0300

    basic reducer

--- client/src/redux/reducer/index.js
+++ client/src/redux/reducer/index.js
@@ -4,8 +4,25 @@ const initialState = {
     users: []
 }
 
+const GET_PRODUCTS = "GET_PRODUCTS";
+const GET_DETAILS =  "GET_DETAILS";
+
 const rootReducer = (state = initialState, action) =>{
-    return state
+    switch(action.type){
+
+
+        case GET_PRODUCTS:
+            return{
+                ...state,
+                products:action.payload,
+            }
+
+        case GET_DETAILS:
+            return{
+                ...state,
+                details:action.payload,
+            }
+    }
 }
 
 export default rootReducer;
