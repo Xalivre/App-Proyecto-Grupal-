a5da4de01450018e436e5649ad2d042fd778ca5b
Ladislao Godocik <ladislaogodocik13@gmail.com>
2022-07-18 21:24:54 -0300

    Header

---
+++ client/src/redux/reducer/index.js
@@ -0,0 +1,11 @@
+const initialState = {
+    products: [],
+    details: [],
+    users: []
+}
+
+const rootReducer = (state = initialState, action) =>{
+    return state
+}
+
+export default rootReducer;
