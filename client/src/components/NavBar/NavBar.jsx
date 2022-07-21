da91d3c919de2c14ffca6d3ae5ad01a331431afd
Picoways <102334887+Picoways@users.noreply.github.com>
2022-07-20 22:31:08 -0300

    Dropdown

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,22 +1,47 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useNavigate, Link } from "react-router-dom";
-import s from "./NavBar.module.css"
+import Style from "./NavBar.module.css"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from "reactstrap"
+import { useDispatch, useSelector } from 'react-redux';
+import { filterPerCategory } from "../../redux/actions/"
 
 function NavBar() {
+
+  const products = useSelector((state) => state.products)
+  const [dropdown, setDropdown] = useState(false)
+  const dispatch = useDispatch()
+
+  const openDropdown = () => {
+    setDropdown(!dropdown)
+  }
+
+  const handleDropdown = (e) => {
+    dispatch(filterPerCategory(e.target.value))
+    console.log(e.target.value)
+  }
+
+
   return (
-    <div className={s.container}>
-        <Link to='/products' style={{textDecoration: 'none'}} className={s.text}>
-            <h1>Productos</h1>
-        </Link>
-        <Link to='/create' style={{textDecoration: 'none'}} className={s.text}>
-            <h1>Arma tu PC</h1>
-        </Link>
-        <Link to='/help' style={{textDecoration: 'none'}} className={s.text}>
-            <h1>Ayuda</h1>
-        </Link>
-        <Link to='/cart' style={{textDecoration: 'none'}} className={s.text}>
-            <h1>Carrito</h1>
-        </Link>
+    <div className={Style.container}>
+      <Dropdown isOpen={dropdown} toggle={openDropdown} size="lr" >
+        <DropdownToggle caret>
+          Categorias
+        </DropdownToggle>
+        <DropdownMenu >
+          {
+            products?.map((e) => <DropdownItem onClick={(e) => handleDropdown(e)}
+            > {e.category}
+            </DropdownItem>)
+          }
+        </DropdownMenu>
+      </Dropdown>
+      <Link to='/create' style={{ textDecoration: 'none' }} className={Style.text}>
+        <h1>Arma tu PC</h1>
+      </Link>
+      <Link to='/help' style={{ textDecoration: 'none' }} className={Style.text}>
+        <h1>Ayuda</h1>
+      </Link>
     </div>
   )
 }
