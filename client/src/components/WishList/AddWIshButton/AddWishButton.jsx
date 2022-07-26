324d47c17418066a8e2c28eeaa83482df3aea8b5
leobravo5 <leandrobravo511@gmail.com>
2022-07-26 13:56:18 -0300

    wishlist and cart bts update

--- client/src/components/WishList/AddWIshButton/AddWishButton.jsx
+++ client/src/components/WishList/AddWIshButton/AddWishButton.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState,useEffect} from 'react'
 import {useDispatch, useSelector} from "react-redux"
 import { addToWishList } from '../../../redux/actions';
 import Style from "./AddWishButton.module.css"
@@ -6,28 +6,50 @@ import FavoriteIcon from '@mui/icons-material/Favorite';
 
 function AddWishButton({id}) {
 
+    const [inList,setInList]=useState(0)
     const dispatch = useDispatch()
     const wishList = useSelector((state)=>state.wishList);
 
-    function addToWish(e){
-        e.preventDefault()
-        let flag=0;
+    useEffect(()=>{
         wishList.forEach(element => {
             if(element._id === id){
-                alert("El producto ya se encuentra en la lista de deseados");
-                flag = 1;
+                setInList(1)
             }
-        });
-        if(!flag){
-            dispatch(addToWishList(id))
-            alert("El Producto fue añadido a la lista")
+        }); 
+    })
+    // function addToWish(e){
+    //     e.preventDefault()
+    //     let flag=0;
+    //     wishList.forEach(element => {
+    //         if(element._id === id){
+    //             alert("El producto ya se encuentra en la lista de deseados");
+    //             flag = 1;
+    //         }
+    //     });
+    //     if(!flag){
+    //         dispatch(addToWishList(id))
+    //         // alert("El Producto fue añadido a la lista")
+    //     }
+    // }
+    function addToWish(e){
+        e.preventDefault()
+        if (inList){
+            alert("El producto ya se encuentra en la lista de deseados")
         }
+        if(!inList){
+            dispatch(addToWishList(id))
+            setInList(1)
+            // alert("El Producto fue añadido a la lista")
+        }
+        console.log(inList);
+        console.log("log de prueba 2")
     }
 
   return (
     <button className={Style.cartButton} onClick={(e) => addToWish(e)}>
         <div className={Style.karting}><FavoriteIcon style={{fontSize:"40px"}}/></div>
-        <div>Añadir a lista de deseados</div>
+        {!inList?(<div>Añadir a lista de deseados</div>):(<div>En lista de deseados</div>)}
+        
     </button>
   )
 }
