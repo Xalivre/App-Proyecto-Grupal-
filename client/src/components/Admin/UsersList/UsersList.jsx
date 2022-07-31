ecd1801d034d4e533c55fcc7107db7464908fc5a
Ariel Salcito <ariel_salcito@live.com>
2022-07-31 17:31:22 -0300

    UpdateUserList

--- client/src/components/Admin/UsersList/UsersList.jsx
+++ client/src/components/Admin/UsersList/UsersList.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 import { getUsers, banUser} from '../../../redux/actions'
 
@@ -7,10 +7,12 @@ export default function UsersList() {
     const dispatch = useDispatch()
 
     const Users = useSelector((state) => state.users.slice(0,5))
+    const [loading, setLoading] = useState(true)
 
     useEffect(() => {
         dispatch(getUsers())
-    }, [])
+        Users.length > 0 && setLoading(false)
+    }, [Users])
 
     const banUserFunction = (e) => {
         let idUser = e.target.value
@@ -20,10 +22,12 @@ export default function UsersList() {
         alert("Usuario baneadon con exito")
     }
 
-    
+
   return (
     <div>
-        <div>
+        {
+         loading === false ?   
+            <div>
             {Users?.map(e => {
                 return  (
                 <div key={e._id}>
@@ -36,7 +40,8 @@ export default function UsersList() {
                 </div>
             )
             })}
-        </div>
+        </div> : <div>Loading...</div>
+        }
     </div>
   )
 }
