b314543c0fcf61e51310c84d1f9b142b4abcf090
Picoways <102334887+Picoways@users.noreply.github.com>
2022-08-04 23:52:04 -0300

    CART

--- client/src/components/PaymentMethod/PaymentMethod.jsx
+++ client/src/components/PaymentMethod/PaymentMethod.jsx
@@ -29,46 +29,47 @@ export default function PaymentMethod() {
       <Link to="/paymentMethod/Checkout"><button>Stripe</button></Link>
       <p> o </p>
       <Link to="/paymentMethod/MercadoPago"><button>Mercadopago</button></Link>
-    <div className={Style.all}>
-      <div className={Style.uno}>
-        <div className={Style.titleCar}>
-          <p className={Style.titlep}>Carrito De Compras</p>
-          <hr />
-        </div>
-        <div className={Style.containerStyles}>
-          <div className={Style.containerCards}>
-            {
-              karting && karting.map(e => {
-                return (
-                  <div className={Style.cartProducts}>
-                    <img className={Style.image} src={e.image[0]?.url} alt=":P" />
-                    <div className={Style.text}>{e.name}</div>
-                    <div className={Style.quantity}>
-                      <button className={Style.menos} onClick={() => modifyQuantityDownFunction(e._id)}>-</button>
-                      <p>{e.quantity}</p>
-                      <button className={Style.mas} onClick={() => modifyQuantityUpFunction(e._id)}>+</button>
+      <div className={Style.all}>
+        <div className={Style.uno}>
+          <div className={Style.titleCar}>
+            <p className={Style.titlep}>Carrito De Compras</p>
+            <hr />
+          </div>
+          <div className={Style.containerStyles}>
+            <div className={Style.containerCards}>
+              {
+                karting && karting.map(e => {
+                  return (
+                    <div className={Style.cartProducts}>
+                      <img className={Style.image} src={e.image[0]?.url} alt=":P" />
+                      <div className={Style.text}>{e.name}</div>
+                      <div className={Style.quantity}>
+                        <button className={Style.menos} onClick={() => modifyQuantityDownFunction(e._id)}>-</button>
+                        <p>{e.quantity}</p>
+                        <button className={Style.mas} onClick={() => modifyQuantityUpFunction(e._id)}>+</button>
+                      </div>
+                      <div className={Style.price}>${e.price * e.quantity}</div>
+                      <DeleteCartButton className={Style.btnX} id={e._id} />
                     </div>
-                    <div className={Style.price}>${e.price * e.quantity}</div>
-                    <DeleteCartButton className={Style.btnX} id={e._id} />
-                  </div>
-                )
-              })
-            }
+                  )
+                })
+              }
+            </div>
           </div>
         </div>
-      </div>
-      <div className={Style.dos}>
-      <div className={Style.compra}>
-        <p className={Style.titlep}>Tu Compra</p>
-        <hr />
-      </div>
-        <div className={Style.paymentButtons}>
-          <p className={Style.positioning}>Total: ${karting?.map(e => (e.price * e.quantity)).reduce((a, b) => a + b, 0)}</p>
-          <Link to="/paymentMethod/Checkout"><button className="button">Stripe</button></Link>
-          <p> o </p>
-          <Link to="#"><button className="button">Mercadopago</button></Link>
+        <div className={Style.dos}>
+          <div className={Style.compra}>
+            <p className={Style.titlep}>Tu Compra</p>
+            <hr />
+          </div>
+          <div className={Style.paymentButtons}>
+            <p className={Style.positioning}>Total: ${karting?.map(e => (e.price * e.quantity)).reduce((a, b) => a + b, 0)}</p>
+            <Link to="/paymentMethod/Checkout"><button className="button">Stripe</button></Link>
+            <p> o </p>
+            <Link to="#"><button className="button">Mercadopago</button></Link>
+          </div>
+          <br />
         </div>
-        <br />
       </div>
     </div>
   )
