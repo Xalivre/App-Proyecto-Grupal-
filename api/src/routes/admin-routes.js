4cd6779a822397add8d11984ff3b94133d8ea824
Picoways <102334887+Picoways@users.noreply.github.com>
2022-08-05 23:05:29 -0300

    DATES <3

--- api/src/routes/admin-routes.js
+++ api/src/routes/admin-routes.js
@@ -1,10 +1,11 @@
 import { Router } from 'express'
-import { forcePasswordAdmin, changeState, searchStatePayment} from "../controllers/adminController.js";
+import { forcePasswordAdmin, changeState, searchStatePayment, searchUserByUsername} from "../controllers/adminController.js";
 
 const router = Router()
 
 router.put('/forcepassword', forcePasswordAdmin);
 router.put('/updatestate', changeState)
 router.get('/filterPaymentState', searchStatePayment)
+router.post('/findUser', searchUserByUsername)
 
 export default router
