2b8e38214418a94bb5a60c0bcee315431f10664e
Juancis <100210614+Juancis@users.noreply.github.com>
2022-08-08 20:10:33 -0300

    Vengan de a 1

--- client/src/components/ProductCard/ProductCard.jsx
+++ client/src/components/ProductCard/ProductCard.jsx
@@ -1,81 +1,70 @@
-import React, { useEffect, useState } from "react";
-import { addToCart, addToWishList, modifyCart, getUserById } from "../../redux/actions";
-import { useDispatch, useSelector } from "react-redux";
-import Style from "./ProductCard.module.css";
+import React, { useEffect } from 'react';
+import {
+  addToCart,
+  addToWishList,
+  modifyCart,
+  getUserById,
+} from '../../redux/actions';
+import { useDispatch, useSelector } from 'react-redux';
+import Style from './ProductCard.module.css';
 import swal from 'sweetalert';
 /* import AddCartButton from "../AddCartButton/AddCartButton"; */
-import { Link } from "react-router-dom";
-import axios from "axios"
-import {useJwt} from "react-jwt"
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import { useJwt } from 'react-jwt';
 /* import WishListButton from "../CardButtons/WishListButton"; */
 
-
-
-
-
-
-
 export default function ProductCard({ id, name, price, image, stock }) {
+  const { decodedToken } = useJwt(localStorage.getItem('usuario'));
 
-  const { decodedToken } = useJwt(localStorage.getItem("usuario"))
-
-  let autho = decodedToken?.role
-  let idUser = decodedToken?._id
+  let autho = decodedToken?.role;
+  let idUser = decodedToken?._id;
 
   const dispatch = useDispatch();
-
-  const [refresh2, setRefresh2] = useState(false)
-
-  const userDetails = useSelector((state) => state.userDetails)
-
-    
-    
-  useEffect(() => {
-    idUser && dispatch(getUserById(idUser))
-    
-  },[idUser])
+  const userDetails = useSelector((state) => state.userDetails);
 
   useEffect(() => {
-   idUser && dispatch(getUserById(idUser))
-   
-  }, [dispatch])
+    idUser && dispatch(getUserById(idUser));
+  }, [idUser]);
 
+  useEffect(() => {
+    idUser && dispatch(getUserById(idUser));
+  }, [dispatch]);
 
-  const karting = useSelector((state) => state.cart)
+  const karting = useSelector((state) => state.cart);
 
-
-
-  const f = localStorage.getItem("Carrito") ? JSON.parse(localStorage.getItem("Carrito")) : []
-
+  const f = localStorage.getItem('Carrito')
+    ? JSON.parse(localStorage.getItem('Carrito'))
+    : [];
 
   const refresh = (f) => {
-    const w = f.filter(e => e?.stock > 0)
-    localStorage.setItem("Carrito", JSON.stringify(w))
-    dispatch(modifyCart(w))
-  }
+    const w = f.filter((e) => e?.stock > 0);
+    localStorage.setItem('Carrito', JSON.stringify(w));
+    dispatch(modifyCart(w));
+  };
 
   useEffect(() => {
-    refresh(f)
+    refresh(f);
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
-  
-  
+  }, []);
+
   const cartStorage = async (id) => {
-    if(autho === "admin" || autho === "owner"){
-      return alert("borrenme algun dia")
+    if (autho === 'admin' || autho === 'owner') {
+      return alert('borrenme algun dia');
     }
-    let json = await axios.get("http://localhost:3000/product/" + id);
-    const a = localStorage.getItem("Carrito") ? JSON.parse(localStorage.getItem("Carrito")) : []
-    console.log(a)
-    a.push(json.data)
-    localStorage.setItem("Carrito", JSON.stringify(a))
-  }
-  
+    let json = await axios.get('http://localhost:3000/product/' + id);
+    const a = localStorage.getItem('Carrito')
+      ? JSON.parse(localStorage.getItem('Carrito'))
+      : [];
+    a.push(json.data);
+    localStorage.setItem('Carrito', JSON.stringify(a));
+  };
+
   return (
     <div className={Style.carouselOrder}>
       <div className={Style.container}>
         <div className={Style.cardContainer}>
-          <Link to={"/product/" + id}>
+          <Link to={'/product/' + id}>
             <br />
             <img className={Style.image} src={image} alt="img" />
             <br /> <br />
@@ -87,30 +76,84 @@ export default function ProductCard({ id, name, price, image, stock }) {
                   <p>{name}</p>
                 )}
               </div>
-            </div >
+            </div>
             <p className={Style.price}>${price}</p>
           </Link>
-
         </div>
         <br />
         <div className={Style.buttonsContainer}>
-          {
-           stock > 0 ? <button onClick={() => {
-            (autho === "admin" || autho === "owner") ? swal("Error", "Un administrador no puede realizar esta acción", "error") :
-            !karting.map((a) => a._id).includes(id) ? dispatch(addToCart(id)) && cartStorage(id)
-             : swal("Oops","Este producto ya se encuentra en tu carrito","warning")
-          }}
-            className="button">Añadir al carrito</button> : <button className="noStockButton" onClick={() => {swal("Sin Stock!","Este producto no tiene stock disponible","error"); console.log(stock)}}>Sin Stock</button>
-            }
+          {stock > 0 ? (
+            <button
+              onClick={() => {
+                autho === 'admin' || autho === 'owner'
+                  ? swal(
+                      'Error',
+                      'Un administrador no puede realizar esta acción',
+                      'error'
+                    )
+                  : !karting.map((a) => a._id).includes(id)
+                  ? dispatch(addToCart(id)) && cartStorage(id)
+                  : swal(
+                      'Oops',
+                      'Este producto ya se encuentra en tu carrito',
+                      'warning'
+                    );
+              }}
+              className="button"
+            >
+              Añadir al carrito
+            </button>
+          ) : (
+            <button
+              className="noStockButton"
+              onClick={() => {
+                swal(
+                  'Sin Stock!',
+                  'Este producto no tiene stock disponible',
+                  'error'
+                );
+              }}
+            >
+              Sin Stock
+            </button>
+          )}
           <br />
-          {localStorage.getItem("usuario") ? <button onClick={() => {
-          
-             (autho === "admin" || autho === "owner") ? swal("Error", "Un administrador no puede realizar esta acción", "error") :
-            !userDetails.wishList?.map((a) => a._id).includes(id) ? dispatch(addToWishList(id, idUser)) &&
-            
-              swal("Listo!","El producto fue agregado a la lista de deseados","success") : swal("Oops","Este producto ya se encuentra en tu lista","warning")
-          }} className="buttonWishlist">Añadir a lista de deseados</button> :
-            <button onClick={() => swal("Atencion!","Debes logearte para hacer eso!","warning")} className="buttonWishlist">Añadir a lista de deseados</button>}
+          {localStorage.getItem('usuario') ? (
+            <button
+              onClick={() => {
+                autho === 'admin' || autho === 'owner'
+                  ? swal(
+                      'Error',
+                      'Un administrador no puede realizar esta acción',
+                      'error'
+                    )
+                  : !userDetails.wishList?.map((a) => a._id).includes(id)
+                  ? dispatch(addToWishList(id, idUser)) &&
+                    swal(
+                      'Listo!',
+                      'El producto fue agregado a la lista de deseados',
+                      'success'
+                    )
+                  : swal(
+                      'Oops',
+                      'Este producto ya se encuentra en tu lista',
+                      'warning'
+                    );
+              }}
+              className="buttonWishlist"
+            >
+              Añadir a lista de deseados
+            </button>
+          ) : (
+            <button
+              onClick={() =>
+                swal('Atencion!', 'Debes logearte para hacer eso!', 'warning')
+              }
+              className="buttonWishlist"
+            >
+              Añadir a lista de deseados
+            </button>
+          )}
           <br />
         </div>
       </div>
